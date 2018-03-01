644c299a81d4a0026855cd07b2c98697bd6fe592
mac <mac@10.224.240.84>
2018-03-01 14:22:54 +0100

    version 2.0 du beau projet coopératif

---
+++ test/src/test/Fenetre.java
@@ -0,0 +1,11 @@
+package test;
+
+import javax.swing.JFrame;
+
+public class Fenetre extends JFrame {
+	Fenetre() {
+		this.setSize(480, 480);
+		this.setTitle("YACOUME ET NONO 2.0");
+		this.setVisible(true);
+	}
+}
---
+++ test/src/test/main.java
@@ -0,0 +1,9 @@
+package test;
+
+public class main {
+
+	public static void main(String[] args) {
+		Fenetre f = new Fenetre();
+	}
+
+}

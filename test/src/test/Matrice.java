e5e563f3e409e8dfa7e0b2c2c95398799dd4f9c6
Noah <Noah@Noah-PC.home>
2018-03-03 19:01:32 +0100

    Initialiser une ligne d'une matrice

--- test/src/test/Matrice.java
+++ test/src/test/Matrice.java
@@ -1,15 +1,41 @@
 package test;
+
+import java.util.Scanner;
+
 public class Matrice {
 	
+	int m, n, value; // m = nbre de lignes et n = nbre de colonnes
+	int[][] matrice;
+	Scanner scan = new Scanner(System.in);
+	
+	
 	public Matrice (int m, int n) {
-		
-		int[][] matrice = new int[m][n];
+		this.m = m;
+		this.n = n;
+		matrice = new int[m][n];
 		
 		
 	}
 	
-	
-	
+	public void setValue() {
+		System.out.print("Mettez les valeurs par lignes ! ");
+		for (int i=0; i<m; i++) {
+			System.out.print("Insérez un chiffre : ");
+			value = scan.nextInt();
+			matrice[0][i] = value;
+		}
+		
+		// afficher le contenu du tableau matrice
+		for (int sousTab[] : matrice) { 
+			
+			for (int value : sousTab) {
+				System.out.print(value);
+			}
+			
+		}
+		
+		
+	}
 	
 	
 	
--- test/src/test/main.java
+++ test/src/test/main.java
@@ -4,6 +4,8 @@ public class main {
 
 	public static void main(String[] args) {
 		Fenetre f = new Fenetre();
+		Matrice matrice = new Matrice(2,2);
+		matrice.setValue();
 	}
 
 }

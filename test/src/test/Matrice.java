252d58c60a6e6c566193058bafad6974a12982cf
noah <noah@macbook-air-de-noah.home>
2018-03-04 21:12:21 +0100

    lol

--- test/src/test/Matrice.java
+++ test/src/test/Matrice.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Matrice {
 	
 	int m, n, value; // m = nbre de lignes et n = nbre de colonnes
-	int[][] matrice;
+	int[][] matrice, inverse;
 	Scanner scan = new Scanner(System.in);
 	
 	
@@ -14,9 +14,10 @@ public class Matrice {
 		this.m = m;
 		this.n = n;
 		matrice = new int[m][n];
-		
+		inverse = new int[m][n];
 		
 	}
+	
 	
 	public void setValue() {
 		System.out.print("Mettez les valeurs par lignes ! ");
@@ -28,20 +29,37 @@ public class Matrice {
 			}
 		}
 		
-		// afficher le contenu du tableau matrice
-		for (int sousTab[] : matrice) { 
-			
-			for (int value : sousTab) {
-				System.out.print(value);
-			}
-			System.out.println("");
-		}
-		
+		this.showValues();
 		
 	}
 	
+	public void showValues() {
+		// afficher le contenu du tableau matrice
+		for (int sousTab[] : matrice) { 
+			System.out.print("|");
+			for (int value : sousTab) {
+				System.out.print(value);
+			}
+			System.out.println("|");
+		}	
+		
+	}
 	
-	
+	public void calculerInverse () {
+		
+		for (int sousTab[] : inverse) {
+			
+		}
+		
+		for (int sousTab[] : matrice) { 
+			System.out.print("|");
+			for (int value : sousTab) {
+				System.out.print(value);
+			}
+			System.out.println("|");
+		}	
+		
+	}
 	
 	
 	

56f8e5335f95ff960e46fab4a037f0088f9c3799
jakub <jakub@192.168.1.44>
2018-05-21 21:06:23 +0200

    Premier essai implémentation classe main dans JFrame

---
+++ test/src/test/TestSwingMatrice.java
@@ -0,0 +1,44 @@
+package test;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+
+public class TestSwingMatrice extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestSwingMatrice frame = new TestSwingMatrice();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TestSwingMatrice() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(10, 2, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+	}
+
+}
